6d4d892b8fbad7e14d4adb94fea155cc17cbbfbe
blizzardfun <38262823+blizzardfun@users.noreply.github.com>
2019-01-06 21:17:42 -0600

    changed d3.json statement

--- static/js/line.js
+++ static/js/line.js
@@ -9,29 +9,33 @@ var margin = {
     bottom: 60,
     left: 60
 };
+
 // Define dimensions of the chart area
 var chartWidth = svgWidth - margin.left - margin.right;
 var chartHeight = svgHeight - margin.top - margin.bottom;
 
+
 // Select div, append SVG area to it, and set its dimensions
-var svg = d3.select("line")
+var svg = d3.select("#line")
     .append("svg")
     .attr("width", svgWidth)
     .attr("height", svgHeight);
+    
 
 // Append a group area, then set its margins
 var chartGroup = svg.append("g")
     .attr("transform", `translate(${margin.left}, ${margin.top})`);
+
 
 // Configure a parseTime function which will return a new Date object from a string
 var parseTime = d3.timeParse("%Y");
 // var data = [];
 
 // Load data
-d3.json("/citation", function(error, cntData) {
+d3.json("/citation").then(function(cntData) {
 
     // Throw an error if one occurs
-    if (error) throw error;
+    // if (error) throw error;
 
     // Print the citation counts
     console.log(cntData);
