4310a4fac59571304256a232d986b57bde6b41f1
jparrucci <38542296+jparrucci@users.noreply.github.com>
2019-01-10 17:15:41 -0600

    Update piechart1.js

--- static/js/piechart1.js
+++ static/js/piechart1.js
@@ -9,17 +9,20 @@ function buildCharts(newSample) {
         const id = results.OBJECTID;
         const citations = results.citationIssued;
         const gender = results.gender;
-        console.log(id,citations,gender)
+        // console.log(id,citations,gender)
 
         var data = [{
-          labels: id.slice(0,10),
-          values: citations.slice(0,10),
+          labels:citations,
+          values:id,
+          
           type: "pie"
 
       }];
 
       var layout = {
-        margin:{t:0,1:0},
+        margin:{t:0,4:6},
+        height: 500,
+        width: 700,
         title: "Citation Issued Vs Stops",
 
     };
@@ -29,49 +32,24 @@ function buildCharts(newSample) {
 function init()
             {
   var selector=d3.select("#selDataset1");
-  d3.json("/neighborhood").then((sampleNames) => {
+  d3.json("/neighborhoodj").then((sampleNames) => {
   sampleNames.forEach((entry) => {
         selector
         .append("option")
-        .text(entry.neighborhood)
-        .property("value",entry.neighborhood);
+        .text(entry)
+        .property("value",entry);
 });
 
-  const newSample=sampleNames;
+  const newSample=sampleNames[0];
+  console.log(newSample)
   optionChanged(newSample);
   buildCharts(newSample);
 });
 }
 
 function optionChanged(newSample) {
-// console.log(newSample.entry);
 
 }
-
-// Plotly.d3.json(function (error, pieData) {
-
-//   var citationIssued = pieData['citationIssued']
-//   var OBJECTID = pieData['OBJECTID']
-// });
-//   var data = [{
-//       values: OBJECTID,
-//       labels: citationIssued,
-//       type: "pie"
-
-//   }];
-
-//   var layout = {
-//     height: 500,
-//     width: 700,
-//     title: "Citation Issued Vs Stops",
-
-//  };
-
-// Plotly.newPlot("plot1", data, layout);
-
-// // Fetch new data each time a new sample is selected
-// buildMetadata(newSample);
-// buildCharts(newSample);
 
 // // Initialize the dashboard
 init();
